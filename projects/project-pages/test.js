e425349f4be3020616e06d24122f659c222c77ff
mertzk <mertzk@cmc304-01.mathcs.carleton.edu>
2017-05-26 15:47:17 -0500

    testing

--- projects/project-pages/test.js
+++ projects/project-pages/test.js
@@ -1,1 +1,9 @@
 document.getElementById("demo").innerHTML = "Paragraph changed!";
+document.getElementById("title").innerHTML = "Paragraph changed!";
+document.getElementById("image").innerHTML = "<img src='./Analog-Drone.png' alt='HTML tutorial' style='width:42px;height:42px;border:0;''>";
+document.getElementById("demo").innerHTML = "Paragraph changed!";
+<div id="navBar">a</div>
+<div id="title"></div>
+<div id="image"></div>
+<div id="body"></div>
+<div id="demo"></div>
