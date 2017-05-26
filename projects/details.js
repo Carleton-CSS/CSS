260f4c1f30ed3abe7f91026a88f1d47140dcc2f6
mertzk <mertzk@cmc304-01.mathcs.carleton.edu>
2017-05-26 15:35:02 -0500

    testing

--- projects/details.js
+++ projects/details.js
@@ -1,8 +1,8 @@
 var urlParams = new URLSearchParams(window.location.search);
-console.log(urlParams.get('action'));
+var project = urlParams.get('proj'));
 
 var head= document.getElementsByTagName('head')[0];
  var script= document.createElement('script');
  script.type= 'text/javascript';
- script.src= './project-pages/test.js';
+ script.src= './project-pages/'+project+'.js';
  head.appendChild(script);
